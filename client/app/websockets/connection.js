3e1c488eb9e22380ab68644a14ae4975e63d00ac
Hamza <hamzasaliheddine@hotmail.com>
2025-01-24 22:00:42 +0100

    more cleaning

--- client/app/websockets/connection.js
+++ client/app/websockets/connection.js
@@ -1,39 +1,16 @@
-import { myNickname } from "../api/users.js";
-import { targetId } from "../services/users.js";
-import { sendMessage, createUser } from "../utils/helpers.js";
+import {
+  handleIncommingMessage,
+  sendPrivateMessage,
+} from "./handle_connection.js";
 import { nav } from "../utils/navigation.js";
 
 export const connectToServer = () => {
   const conn = new WebSocket("ws://localhost:8080/ws");
+  const sendMessageBtn = document.getElementById("btn-message");
+  const logoutBtn = document.getElementById("logout-button");
   conn.onmessage = (event) => {
     let res = JSON.parse(event.data);
-    switch (res.type) {
-      case "chat":
-        const targetDiv = document.getElementById(res.sender);
-        console.log(targetDiv);
-        
-        if (targetId == res.sender) {
-          sendMessage(res.content, targetDiv.dataset.nickname);
-        } else {
-          targetDiv.classList.add("new-message");
-        }
-        document.querySelector(".users-list").prepend(targetDiv);
-        break;
-
-      case "status":
-        let div = document.getElementById(`status-${res.id}`);
-        if (div) {
-          div.classList.remove("offline", "online");
-          if (res.status) {
-            div.classList.add("online");
-          } else {
-            div.classList.add("offline");
-          }
-        } else {
-          createUser(res);
-        }
-        break;
-    }
+    handleIncommingMessage(res);
   };
 
   conn.onerror = (error) => {
@@ -44,43 +21,13 @@ export const connectToServer = () => {
     console.log("WebSocket connection closed:", event.code, event.reason);
   };
 
-  document.getElementById("btn-message").addEventListener("click", () => {
-    let message = document.getElementById("message");
-    if (conn.readyState != WebSocket.OPEN) {
-      console.error("WebSocket is closed");
-      return;
-    }
-    if (message.value.length > 500 || !message.value.trim()) {
-      message.classList.add("error");
-      setTimeout(() => {
-        message.classList.remove("error");
-      }, 1000);
-      return;
-    }
-    if (
-      document
-        .getElementById(`status-` + targetId)
-        .classList.contains("offline")
-    ) {
-      message.classList.add("error");
-      setTimeout(() => {
-        message.classList.remove("error");
-      }, 1000);
-    } else {
-      message.classList.remove("error");
-      sendMessage(message.value, myNickname);
-      document
-        .querySelector(".users-list")
-        .prepend(document.getElementById(targetId));
-      conn.send(JSON.stringify({ target: targetId, content: message.value }));
-      message.value = "";
-    }
+  sendMessageBtn.addEventListener("click", () => {
+    sendPrivateMessage(conn);
   });
-  document
-    .getElementById("logout-button")
-    .addEventListener("click", async () => {
-      conn.close(1000, "logout");
-      await fetch("/logout");
-      nav("/login");
-    });
+
+  logoutBtn.addEventListener("click", async () => {
+    conn.close(1000, "logout");
+    await fetch("/logout");
+    nav("/login");
+  });
 };
---
+++ client/app/websockets/handle_connection.js
@@ -0,0 +1,64 @@
+import { targetId } from "../services/users.js";
+import { sendMessage, createUser } from "../utils/helpers.js";
+import { myNickname } from "../api/users.js";
+
+export const handleIncommingMessage = (msg) => {
+  switch (msg.type) {
+    case "chat":
+      const targetDiv = document.getElementById(msg.sender);
+      console.log(targetDiv);
+
+      if (targetId == msg.sender) {
+        sendMessage(msg.content, targetDiv.dataset.nickname);
+      } else {
+        targetDiv.classList.add("new-message");
+      }
+      document.querySelector(".users-list").prepend(targetDiv);
+      break;
+
+    case "status":
+      let div = document.getElementById(`status-${msg.id}`);
+      if (div) {
+        div.classList.remove("offline", "online");
+        if (msg.status) {
+          div.classList.add("online");
+        } else {
+          div.classList.add("offline");
+        }
+      } else {
+        createUser(msg);
+      }
+      break;
+  }
+};
+
+export const sendPrivateMessage = (conn) => {
+  let message = document.getElementById("message");
+  if (conn.readyState != WebSocket.OPEN) {
+    console.error("WebSocket is closed");
+    return;
+  }
+  if (message.value.length > 500 || !message.value.trim()) {
+    message.classList.add("error");
+    setTimeout(() => {
+      message.classList.remove("error");
+    }, 1000);
+    return;
+  }
+  if (
+    document.getElementById(`status-` + targetId).classList.contains("offline")
+  ) {
+    message.classList.add("error");
+    setTimeout(() => {
+      message.classList.remove("error");
+    }, 1000);
+  } else {
+    message.classList.remove("error");
+    sendMessage(message.value, myNickname);
+    document
+      .querySelector(".users-list")
+      .prepend(document.getElementById(targetId));
+    conn.send(JSON.stringify({ target: targetId, content: message.value }));
+    message.value = "";
+  }
+};
